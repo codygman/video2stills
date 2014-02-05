edb3148b5ae47ca4f0db3d4ecaa4ec84e90f8574
Cody Goodman <codygman.consulting@gmail.com>
2014-02-05 04:04:07 -0600

    Return filepath of generated stills or error message

--- ffmpeg.hs
+++ ffmpeg.hs
@@ -1,18 +1,34 @@
+import Data.Maybe
+import System.Directory
 import System.IO (IOMode (ReadMode), Handle, hPutStrLn, stderr, stdout, hClose, openFile)
+import System.IO.Error
+import System.Environment (getArgs)
 import System.Process
 import System.IO.Temp
-import System.Directory
 import Text.Printf (printf)
 import System.Exit (ExitCode(..))
+import System.Console.GetOpt
+
+randomDirectory :: IO FilePath
+randomDirectory = createTempDirectory "/tmp" "stills"
 
 main :: IO ()
 main = do
-  videoh <- openFile "DSCF2022.AVI" ReadMode
-  withSystemTempDirectory "stills" $ \tmpDir -> do
-    genStills videoh tmpDir
-    getDirectoryContents tmpDir >>= print
+  args <- getArgs
+  let (actions, nonOptions, _) = getOpt RequireOrder [] args
+  case (listToMaybe nonOptions) of
+    Just arguments -> do 
+         let videoPath = head nonOptions
+         videoh <- openFile videoPath ReadMode
+         tmpDir <- createTempDirectory "/tmp" "stills"
+         result <- genStills videoh tmpDir
+         case result of
+           Right p -> putStrLn $ "Succesfully generated stills at: " ++ p
+           Left e -> putStrLn $ "error generating video stills: " ++ e
 
-genStills :: Handle -> FilePath -> IO ()
+    Nothing -> putStrLn "no video path given"
+
+genStills :: Handle -> FilePath -> IO (Either String FilePath)
 genStills videoh tmpDir = do
     let videoStdin = UseHandle videoh
         fullPath = tmpDir
@@ -22,34 +38,11 @@ genStills videoh tmpDir = do
                             std_err = CreatePipe}
     exit <- waitForProcess pHandle
     hClose videoh
-    case exit of
+    return $ case exit of
       ExitSuccess ->
-        putStrLn $ "successfully wrote video stills to "  ++ tmpDir
+          Right  tmpDir
       ExitFailure code ->
-        hPutStrLn stderr $ "video2stills script exited with non-zero exit code: " ++ show code
+          Left $ "Script exited with non-zero exit code: " ++ show code
   where
     arguments = words $ "-i - -r 3 -f image2 " ++ tmpDir ++ "/DSCF2022.AVI-%03d.jpg"
-    ffmpeg = (proc "ffmpeg" arguments)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- TODO
--- <geekosaur> I would also note you're still doing questionable things;
---     reading from the handles should be done in separate threads, and
---     you should waitForProcess in the main thread.
-
+    ffmpeg = proc "ffmpeg" arguments
